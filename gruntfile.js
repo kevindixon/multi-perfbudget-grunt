e36cbd518187da68d11ee4962bc7d6fd7f40a0c2
Kevin Dixon <kevinrdixon@gmail.com>
2015-01-30 12:12:55 +0000

    If any of the configuration files are missing, then the relevant task will complete successfully. This way can test only a subset of webpagetest/yslow/pagespeed

--- gruntfile.js
+++ gruntfile.js
@@ -2,14 +2,18 @@
 * Grunt definition
 */
 module.exports = function(grunt) {
-	// Time the execution of the task
-	var timer = require("grunt-timer");
-	timer.init(grunt);
+	/* 
+	If there is no ySlow, PageSpeed or WPT configuration file, pass in a
+	null task config - will run successfully
+	*/
+	var pageSpeedConfig = grunt.file.exists('pagespeed.json') ? grunt.file.readJSON('pagespeed.json') : {"options": {}};
+	var yslowConfig = grunt.file.exists('yslow.json') ? grunt.file.readJSON('yslow.json') : {"options": {}};
+	var wptConfig = grunt.file.exists('webpagetest.json') ? grunt.file.readJSON('webpagetest.json') : {"default": {}};
 
 	// Project configuration.
 	grunt.initConfig({
-		pagespeed: grunt.file.readJSON('pagespeed.json'),
-		yslow: grunt.file.readJSON('yslow.json')
+		pagespeed: pageSpeedConfig,
+		yslow: yslowConfig
 	});
 
 	// Load dependency tasks 
@@ -17,7 +21,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-pagespeed');
 	grunt.loadNpmTasks('grunt-perfbudget');
 
-	grunt.registerMultiTask('webpagetest-run', 'Run webpage tests', function() {
+	grunt.registerMultiTask('webpagetest-run', 'Run WebPageTest tests', function() {
 		grunt.log.writeln("Checking URL "+this.data.default.options.url);
 		grunt.config('perfbudget', this.data);
 		grunt.task.run('perfbudget');
@@ -26,13 +30,12 @@ module.exports = function(grunt) {
 	/*
 	* Task to loop through tests in webpagetest.json
 	*/
-	grunt.registerTask('webpagetest', 'Run a set of webpage tests', function() {
-		var tests = grunt.file.readJSON('webpagetest.json');
-		var defaults = tests.default;
+	grunt.registerTask('webpagetest', 'Run a set of WebPageTest tests', function() {
+		var defaults = wptConfig.default;
 		if (typeof(defaults) == 'undefined') {
 			grunt.fail.warn("No webpagetest defaults defined");
 		}
-		var keys = Object.keys(tests);
+		var keys = Object.keys(wptConfig);
 		for (var count = 0; count < keys.length; ++count) {
 			var testName = keys[count];
 			if (testName == 'default') {
@@ -42,7 +45,7 @@ module.exports = function(grunt) {
 			// Merge defaults into the test definition
 			var mergedTest = {};
 			deepExtend(mergedTest, defaults);
-			deepExtend(mergedTest, tests[keys[count]]);
+			deepExtend(mergedTest, wptConfig[keys[count]]);
 			var test = {'default': JSON.parse(JSON.stringify(mergedTest))};
 			// Queue webpagetest to run in sequence when this task is done
 			grunt.config('webpagetest-run.'+testName, test);
